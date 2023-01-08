3a56a3f311a65e019bd8276efd4f4843836d1632
xJxckk <17519346+xjxckk@users.noreply.github.com>
2023-01-08 13:01:35 +0100

    Logger updates

--- printr/__init__.py
+++ printr/__init__.py
@@ -1,20 +1,81 @@
+import sys, logging, coloredlogs, json, os, shutil
+from pathlib import Path
 from datetime import datetime
-import json, shutil
+
+class logger:
+    def __init__(self, filename=None, max_filesize=1024):
+        if not filename:
+            filename = Path(sys.argv[0]).stem
+        self.filename = filename
+        self.max_filesize = max_filesize
+
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+
+        log_file = logging.FileHandler(filename, mode='w')
+        log_format = logging.Formatter(fmt='%(levelname)s - %(asctime)s.%(msecs)03d - Line %(lineno)s: %(message)s', datefmt='%H:%M:%S')
+        log_file.setFormatter(log_format)
+        logger.addHandler(log_file)
+        self.log_file = log_file
+
+        coloredlogs.install(level=logging.INFO, logger=logger, fmt='%(message)s')
+        self.logger = logger
+
+    def log(self, *items, level='info'):
+        message = ''
+        for item in items:
+            if message and '\n' not in message:
+                message += ' ' # Add space in between variables
+            if isinstance(item, dict) or isinstance(item, list):
+                try:
+                    if message and '\n' not in message: # Add line breaks before and after
+                        formatted_item = '\n'
+                    else:
+                        formatted_item = ''
+                    formatted_item += json.dumps(item, indent=4) # Beautify JSON objects
+                    item = formatted_item + '\n'
+                except TypeError:
+                    pass
+            message += str(item)
+
+        if level == 'info':
+            self.logger.info(message)
+        elif level == 'debug':
+            self.logger.debug(message)
+        elif level == 'error':
+            self.logger.error(message)
+
+        if os.path.getsize(self.filename) > self.max_filesize:
+            self.logger.info('Resetting log file')
+            log_file.close()
+    
+    def error(self, *items):
+        self.log(*items, level='error')
+    
+    def debug(self, *items):
+        self.log(*items, level='debug')
+    
+    def current_time(self, *items):
+        coloredlogs.install(level=logging.INFO, logger=self.logger, fmt='%(asctime)s.%(msecs)03d: %(message)s', datefmt='%H:%M:%S') # Add curent time to printed log output format
+        self.log(*items)
+        coloredlogs.install(level=logging.INFO, logger=self.logger, fmt='%(message)s') # Reset to default log format
 
 class printr:
     '''printr'''
-    def __init__(self, *items, same_line=False, current_time=False):
+    def __init__(self, *items, same_line=False, current_time=False, level='info'):
         if len(items) == 1:
             message = items[0]
             if isinstance(message, dict) or isinstance(message, list):
-                message = json.dumps(message, indent=4) # Beautify JSON objects
+                try: message = json.dumps(message, indent=4) # Beautify JSON objects
+                except TypeError: pass
         else:
             message = ''
             for item in items:
                 if message:
                     message += ' ' # Add space in between variables
                 if isinstance(item, dict) or isinstance(item, list):
-                    item = json.dumps(item, indent=4)
+                    try: item = json.dumps(item, indent=4) # Beautify JSON objects
+                    except TypeError: pass
                 message += str(item)
         if current_time:
             current_time = datetime.now()
@@ -28,7 +89,16 @@ class printr:
             print(message, end='')
             print('\r', end='')
         else:
-            print(message)
+            logger = logging.getLogger()
+            if logger.level != 30:
+                if level == 'debug':
+                    logger.debug(message)
+                elif level == 'error':
+                    logger.error(message)
+                else:
+                    logger.info(message)
+            elif level != 'debug':
+                print(message)
 
 class current_time:
     def __init__(self, *items, same_line=False):
