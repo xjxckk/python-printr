279ae482ef55e33e5b33ea83db70f3442bab973c
j <>
2024-01-24 18:43:46 +0000

    3.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'python-printr',
     packages = ['printr'],
-    version = '3.7',
+    version = '3.8',
     install_requires=['coloredlogs'],
     description = 'printr',
     url = 'https://github.com/xjxckk/python-printr/',
